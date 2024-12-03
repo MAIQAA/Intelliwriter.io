3020637f5a5bd32ab9dba43e2bd741c3c60a2b95
Ammad Rana <ammadrana863@gmail.com>
2024-12-03 22:20:04 +0500

    Buttons Improved

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -255,7 +255,7 @@ const Header = () => {
 
         {/* BUTTONS */}
         <div className="mt-16 flex justify-center w-full">
-          <div className="grid space-y-5 w-full text-white font-medium md:w-1/2">
+          <div className="grid space-y-5 w-full text-white font-medium md:w-2/3">
             <NavLink to="/register">
               <button className="bg-gradient-to-r from-[#6A25BE] to-[#511F8F] rounded-full px-6 py-2 w-full">
                 Register
